0d029fd0a98008dd8a49b2416019ee7306c82c77
BIN <55619475+JAVA-BIN@users.noreply.github.com>
2022-08-24 02:21:48 +0900

    Updates

---
+++ precache-manifest.cde3b234d0fe70bfe7eb6c8ced2de5d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05df6d8fff53aa16a6c8f19152983243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dadd5af3984592856ae4",
+    "url": "/static/css/main.c91cec27.chunk.css"
+  },
+  {
+    "revision": "ae3d3b94af314f27e305",
+    "url": "/static/js/2.5460271d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5460271d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadd5af3984592856ae4",
+    "url": "/static/js/main.f067bc22.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "dc230c3f44f0b3414488e6cba7a803e6",
+    "url": "/static/media/bin.dc230c3f.jpeg"
+  }
+]);
