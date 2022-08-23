e5f7b834d5cdb34a296a14591c871c05620370d2
BIN <55619475+JAVA-BIN@users.noreply.github.com>
2022-08-24 02:21:20 +0900

    Updates

---
+++ precache-manifest.18f005f13718d0d0e31cc7417ea468ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09e3d1d969c2e9ecd30cc7795b1228f",
+    "url": "/ReactFolio/index.html"
+  },
+  {
+    "revision": "dadd5af3984592856ae4",
+    "url": "/ReactFolio/static/css/main.c91cec27.chunk.css"
+  },
+  {
+    "revision": "ae3d3b94af314f27e305",
+    "url": "/ReactFolio/static/js/2.5460271d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactFolio/static/js/2.5460271d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadd5af3984592856ae4",
+    "url": "/ReactFolio/static/js/main.f067bc22.chunk.js"
+  },
+  {
+    "revision": "cc897b93664c64c1287e",
+    "url": "/ReactFolio/static/js/runtime-main.20217e57.js"
+  },
+  {
+    "revision": "dc230c3f44f0b3414488e6cba7a803e6",
+    "url": "/ReactFolio/static/media/bin.dc230c3f.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cde3b234d0fe70bfe7eb6c8ced2de5d1.js"
+  "/ReactFolio/precache-manifest.18f005f13718d0d0e31cc7417ea468ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
